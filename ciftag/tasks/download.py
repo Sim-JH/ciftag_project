97f3c1ce08c8dfd4eb2ed649b10951acf078fcba
Sim-JH <saide0032@gmail.com>
2024-10-07 18:28:35 +0900

    feat: downlaod filter task 구현

--- ciftag/tasks/download.py
+++ ciftag/tasks/download.py
@@ -26,7 +26,7 @@ logs = logger.Logger(log_dir='Download')
 def download_images_by_target(self, target, records, success_list=None, api_proxies=None, ext='png'):
     """requests 기반 타겟별 이미지 다운로드. 재시도 최대 3회"""
     # TODO 필터링 프리셋
-    logs.log_data(f'--- Target: {target} {self.request.retries}회 다운로드 시작. 총 목표 갯수: {len(records)}')
+    logs.log_data(f'--- Target: {target} {self.request.retries+1}회 다운로드 시작. 총 목표 갯수: {len(records)}')
 
     # 이미지를 저장할 디렉토리
     base_dir = os.path.join(f'{conf.get('dir', 'img_dir')}/Target', target)
@@ -104,12 +104,12 @@ def download_images_by_target(self, target, records, success_list=None, api_prox
                     logs.log_data(f'Unexpect Request Error: {e}')
                     retry_list.append(record)
     
-    logs.log_data(f'--- Target: {target} {self.request.retries}회 {len(success_list)}개 다운로드')
+    logs.log_data(f'--- Target: {target} {self.request.retries+1}회 {len(success_list)}개 다운로드')
     
     if len(retry_list) > 0:
         try:
             self.retry(
-                exc=Exception(f"Retrying due to retry list for {self.request.retries} times"), countdown=60,
+                exc=Exception(f"Retrying due to retry list for {self.request.retries+1} times"), countdown=60,
                 kwargs={
                     'target': target,
                     'records': retry_list,
@@ -128,101 +128,101 @@ def download_images_by_target(self, target, records, success_list=None, api_prox
         }
 
 
-# @app.task(bind=True, name="ciftag.task.download_images_by_tags", max_retries=3)
-# def download_images_by_tags(
-#         self, mode, tags, zip_path, records, threshold, success_list=None, api_proxies=None, ext='png'
-# ):
-# def download_images_by_tags(
-#         mode, tags, zip_path, records, threshold, success_list=None, api_proxies=None, ext='png'
-# ):
-#     """requests 기반 태그별 이미지 다운로드. 필터 적용 가능. 재시도 최대 3회"""
-#     # logs.log_data(f'--- Tags: {tags} {self.request.retries}회 다운로드 시작')
-#
-#     # 이미지를 저장할 디렉토리
-#     base_dir = os.path.join(f'{conf.get('dir', 'img_dir')}/Tags', '_'.join(tags))
-#     os.makedirs(base_dir, exist_ok=True)
-#
-#     if success_list is None:
-#         success_list = []
-#
-#     retry_list = []
-#     fail_list = []
-#
-#     # 이미지 필터 모듈 초기화
-#     image_filter = ImageFilter(
-#         sample_image_path=zip_path,
-#         target_tags=tags,
-#         threshold=threshold,
-#         mode='byte'
-#     )
-#
-#     image_sources = []
-#
-#     for idx, record in enumerate(records):
-#         # 이미지 URL에서 파일 확장자 추출할 경우
-#         tag = '_'.join(tags)
-#         crawl_idx, info_idx, data_idx, image_url = record.values()
-#
-#         # 이미지 파일명 생성
-#         filename = f"{tag}_{crawl_idx}_{info_idx}_{data_idx}"
-#
-#         # 이미지 Get
-#         try:
-#             # 세션 연결 실패시 총 3회 재시도
-#             response = make_requests(
-#                 url=image_url,
-#                 api_proxies=api_proxies,
-#                 method='GET',
-#                 params=None,
-#                 headers=None
-#             )
-#
-#             if response.status_code == 200:
-#                 image_sources.append((filename, response.content))
-#
-#         except Exception as e:
-#             if isinstance(e, requests.exceptions.HTTPError):
-#                 if e.response.status_code in [403, 429]:
-#                     retry_list.append(record)
-#                 else:
-#                     retry_list.append(record)
-#             else:
-#                 if isinstance(e, requests.exceptions.ReadTimeout):
-#                     retry_list.append(record)
-#                 elif isinstance(e, requests.exceptions.ProxyError):
-#                     retry_list.append(record)
-#                 else:
-#                     logs.log_data(f'Unexpect Request Error: {e}')
-#                     retry_list.append(record)
-#
-#     filtered_image_source = image_filter.combined_filtering(mode, image_sources)
-#
-#     for file_name, image_source in filtered_image_source:
-#         with open(os.path.join(base_dir, file_name), 'wb') as f:
-#             f.write(image_source)
+@app.task(bind=True, name="ciftag.task.download_images_by_tags", max_retries=3)
+def download_images_by_tags(
+        self, tags, zip_path, records, threshold, filtered_list=None, api_proxies=None, ext='png'
+):
+    """requests 기반 태그별 이미지 다운로드. 필터 적용 가능. 재시도 최대 3회"""
+    logs.log_data(f'--- Tags: {tags} {self.request.retries+1}회 다운로드. 시작 총 목표 갯수: {len(records)}')
 
-    # logs.log_data(f'--- Tags: {tags} {self.request.retries}회 {len(success_list)}개 다운로드')
+    # 이미지를 저장할 디렉토리
+    base_dir = os.path.join(f'{conf.get('dir', 'img_dir')}/Tags', tags.replace('/', '_'))
+    os.makedirs(base_dir, exist_ok=True)
 
-    # if len(retry_list) > 0:
-    #     try:
-    #         self.retry(
-    #             exc=Exception(f"Retrying due to retry list for {self.request.retries} times"), countdown=60,
-    #             kwargs={
-    #                 'mode': mode,
-    #                 'tags': "/".join(tags),
-    #                 'zip_path': zip_path,
-    #                 'records': records,
-    #                 'threshold': threshold,
-    #                 'success_list': success_list,
-    #                 'api_proxies': api_proxies,
-    #                 'ext': ext
-    #             }
-    #         )  # 차단 시 재시도
-    #     except self.MaxRetriesExceededError:
-    #         logs.log_data(f"Max retries exceeded for record {fail_list}", "error")
-    # else:
-    #     # 추후 callback 사용 시
-    #     logs.log_data(f'--- Tags: {tags} 다운로드 종료')
-    #     return {
-    #         'success_list': success_list
-    #     }
+    if filtered_list is None:
+        filtered_list = []
+
+    retry_list = []
+    fail_list = []
+
+    # 이미지 필터 모듈 초기화
+    image_filter = ImageFilter(
+        sample_image_path=zip_path,
+        threshold=threshold,
+    )
+
+    image_sources = []
+
+    for idx, record in enumerate(records):
+        # 이미지 URL에서 파일 확장자 추출할 경우
+        tag = tags.replace('/', '_')
+        crawl_idx, info_idx, data_idx, image_url = record.values()
+
+        # 이미지 파일명 생성
+        filename = f"{tag}_{crawl_idx}_{info_idx}_{data_idx}.{ext}"
+
+        # 이미지 Get
+        try:
+            # 세션 연결 실패시 총 3회 재시도
+            response = make_requests(
+                url=image_url,
+                api_proxies=api_proxies,
+                method='GET',
+                params=None,
+                headers=None
+            )
+            status_code = response.status_code
+
+            if status_code == 200:
+                image_sources.append((filename, response.content))
+            elif status_code == 404:
+                # TODO Not Found 일 때의 DB 처리
+                logs.log_data(f'Not Found Error: {image_url}')
+                continue
+
+        except Exception as e:
+            if isinstance(e, requests.exceptions.HTTPError):
+                if e.response.status_code in [403, 429]:
+                    retry_list.append(record)
+                else:
+                    retry_list.append(record)
+            else:
+                if isinstance(e, requests.exceptions.ReadTimeout):
+                    retry_list.append(record)
+                elif isinstance(e, requests.exceptions.ProxyError):
+                    retry_list.append(record)
+                else:
+                    logs.log_data(f'Unexpect Request Error: {e}')
+                    retry_list.append(record)
+
+    filtered_image_source = image_filter.combined_filtering('byte', image_sources)
+
+    for file_name, image_source in filtered_image_source:
+        with open(os.path.join(base_dir, file_name), 'wb') as f:
+            filtered_list.append(file_name)
+            f.write(image_source)
+
+    logs.log_data(f'--- Tags: {tags} {self.request.retries+1}회 {len(filtered_list)}개 필터링 완료 후 다운로드')
+
+    if len(retry_list) > 0:
+        try:
+            self.retry(
+                exc=Exception(f"Retrying due to retry list for {self.request.retries+1} times"), countdown=60,
+                kwargs={
+                    'tags': "/".join(tags),
+                    'zip_path': zip_path,
+                    'records': records,
+                    'threshold': threshold,
+                    'filtered_list': filtered_list,
+                    'api_proxies': api_proxies,
+                    'ext': ext
+                }
+            )  # 차단 시 재시도
+        except self.MaxRetriesExceededError:
+            logs.log_data(f"Max retries exceeded for record {fail_list}", "error")
+    else:
+        # 추후 callback 사용 시
+        logs.log_data(f'--- Tags: {tags} 다운로드 종료')
+        return {
+            'filtered_list': filtered_list
+        }
