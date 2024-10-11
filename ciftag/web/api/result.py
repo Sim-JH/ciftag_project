c067b89803e3ef673a78631edc746d9601d4a835
Sim-JH <saide0032@gmail.com>
2024-10-11 14:13:51 +0900

    fix: naming 수정

--- ciftag/web/api/result.py
+++ ciftag/web/api/result.py
@@ -8,29 +8,29 @@ from ciftag.web.schemas.result import (
     CrawlDataResponse
 )
 from ciftag.web.crud.result import (
-    get_cred_info_service
+    get_target_crawl_info_service
 )
 
 router = APIRouter()
 
 
 @router.get("/{target_code}/{user_pk}", response_model=List[CrawlInfoResponse])
-async def get_target_info(
+async def get_target_crawl_info(
     target_code: str = Path(title="대상 사이트 코드", description='0 입력 시 전체 조회'),
     user_pk: int = Path(title="사용자 id", description="0 입력 시 전체 조회"),
 ):
     """타겟별 크롤링 정보 조회"""
-    return await get_cred_info_service(target_code, user_pk)
+    return await get_target_crawl_info_service(target_code, user_pk)
 
 
-# @router.get("/img/{target_code}/{user_pk}", response_model=List[CrawlDataResponse])
-# async def get_result_img(
-#     target_code: str = Path(title="대상 사이트 코드", description='0 입력시 전체 조회'),
-#     user_pk: int = Path(title="사용자 id", description="0 입력 시 전체 조회"),
-#     min_height: Union[str, None] = Query(default=None, title="조회할 이미지 최소 높이"),
-#     max_height: Union[str, None] = Query(default=None, title="조회할 이미지 최대 높이"),
-#     max_width: Union[str, None] = Query(default=None, title="조회할 이미지 최대 너비"),
-# ):
-#     """타겟별 수집 목록 조회"""
-#     pass
-#
+@router.get("/img/{target_code}/{user_pk}", response_model=List[CrawlDataResponse])
+async def get_result_img(
+    target_code: str = Path(title="대상 사이트 코드", description='0 입력시 전체 조회'),
+    user_pk: int = Path(title="사용자 id", description="0 입력 시 전체 조회"),
+    min_height: Union[str, None] = Query(default=None, title="조회할 이미지 최소 높이"),
+    max_height: Union[str, None] = Query(default=None, title="조회할 이미지 최대 높이"),
+    max_width: Union[str, None] = Query(default=None, title="조회할 이미지 최대 너비"),
+):
+    """타겟별 수집 목록 조회"""
+    pass
+
--- ciftag/web/crud/result.py
+++ ciftag/web/crud/result.py
@@ -20,6 +20,46 @@ from ciftag.web.crud.core import (
 from sqlalchemy import union_all, text
 
 
+async def get_target_crawl_info_service(target_code: str, user_pk: int):
+    dbm = DBManager()
+
+    with dbm.create_session() as session:
+        def base_info_query(info_model):
+            info_columns = list(info_model.__table__.columns)
+
+            sub_query = session.query(
+                *info_columns,
+                CrawlRequestInfo.user_pk,
+                CrawlRequestInfo.run_on,
+                CrawlRequestInfo.target_code
+            ).join(
+                CrawlRequestInfo, CrawlRequestInfo.id == info_model.crawl_pk
+            )
+
+            if user_pk:
+                sub_query = sub_query.filter(
+                    CrawlRequestInfo.user_pk == user_pk
+                )
+
+            return sub_query
+
+        if target_code == "0":
+            queries = [base_info_query(model) for model in (PinterestCrawlInfo, TumblrCrawlInfo, FlickrCrawlInfo)]
+            query = queries.pop().union_all(*queries)
+        elif target_code == "1":
+            query = base_info_query(PinterestCrawlInfo)
+        elif target_code == "2":
+            query = base_info_query(TumblrCrawlInfo)
+        elif target_code == "3":
+            query = base_info_query(FlickrCrawlInfo)
+        else:
+            raise CiftagAPIException('Target Not Exist', 404)
+
+        records = query.all()
+
+    return records
+
+
 async def get_cred_info_service(target_code: str, user_pk: int):
     dbm = DBManager()
 
